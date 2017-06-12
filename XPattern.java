a5c7f4e80be6c6a7a63a362d631488d9c3825e5e
geeknaveen <geeknaveen@users.noreply.github.com>
2017-06-13 00:30:47 +0530

    Add files via upload

---
+++ XPattern.java
@@ -0,0 +1,50 @@
+package asst2_3;
+
+public class XPattern {
+
+	public static void main(String[] args) {
+		
+		int i =1;
+				
+				for(i=1;i<=5;i++)
+				{
+					for(int j=1;j<=5;j++)
+					{
+						if(i==j)
+						{
+							System.out.print(" *");
+																				
+						}
+					
+						if(i==1 && j==5)
+						{
+							System.out.print("* ");
+													
+						}
+						if(i==2 && j==4)
+						{
+							System.out.print("* ");
+							
+						}	
+					
+						if(i==4 && j==2)
+						{
+							System.out.print("*");
+							
+						}
+						if(i==5 && j==1)
+						{
+							System.out.print("* ");
+							
+						}
+						else
+						{
+							System.out.print(" ");
+													}
+					}System.out.println();
+					
+				}
+
+			}
+
+		}
